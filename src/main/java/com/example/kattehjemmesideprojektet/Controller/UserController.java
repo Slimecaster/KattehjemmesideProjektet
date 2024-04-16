40737cee48ee90ea76e852c2341ac2558a0bb2a1
Slimecaster <nd070503@gmail.com>
2024-04-16 13:05:48 +0200

    hi

--- src/main/java/com/example/kattehjemmesideprojektet/Controller/UserController.java
+++ src/main/java/com/example/kattehjemmesideprojektet/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.kattehjemmesideprojektet.Controller;
 
 import com.example.kattehjemmesideprojektet.Model.User;
 import com.example.kattehjemmesideprojektet.Service.KattehjemmesideService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    @Autowired
     private KattehjemmesideService kattehjemmesideService;
 
     @GetMapping("/")
